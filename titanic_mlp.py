8f875512de80e14e7d8cbe7e0667322c773506e6
phanyasit <punyasit@hotmail.com>
2024-05-23 06:31:20 +0700

    new files

--- titanic_mlp.py
+++ titanic_mlp.py
@@ -6,23 +6,25 @@ from torch import nn
 class Classifier(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(4, 128)
-        self.fc2 = nn.Linear(128, 3)
+        self.fc1 = nn.Linear(12, 128)
+        self.fc2 = nn.Linear(128, 1)
     def forward(self, x):
         x = torch.relu(self.fc1(x))
-        return torch.softmax(self.fc2(x), dim=1)
+        return torch.sigmoid(self.fc2(x))
 
 model = Classifier()
-model.load_state_dict(torch.load('model.pth'))
+model.load_state_dict(torch.load('model_titanic'))
 model.eval()
 
-st.title('Iris Dataset')
+st.title('Titanic Dataset')
 st.sidebar.title('Input Parameters')
 x = np.array([0.] * 4)
-for i, col in enumerate(['sepal_length', 'sepal_width', 'petal_length', 'petal_width']):
+for i, col in enumerate(['survived', 'pclass', 'age', 'sibsp', 'parch', 'fare', 'sex_male',
+       'embarked_C', 'embarked_Q', 'embarked_S', 'who_child', 'who_man',
+       'who_woman']):
     x[i] = st.sidebar.slider(col, 0, 8, 0)
 st.write(x)
-labels = ['setosa', 'versicolor', 'virginica']
+labels = ['รอด', 'ไม่รอด']
 with torch.no_grad():
     y_ = model(torch.tensor([x], dtype=torch.float32))
 st.write('Predicted class:', labels[y_.argmax(dim=1)[0]])
